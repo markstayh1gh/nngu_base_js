e5d314eaca2a27e1b3af5c7eb1907b27f476c159
Mark <egorowmarck@yandex.ru>
2022-09-24 10:41:23 +0300

    First lesson commit

---
+++ script.js
@@ -0,0 +1,15 @@
+'use strict';
+
+//console.log('123');
+
+
+const ourPlanet = 'Earth',
+      currentUserTime = '10',
+      currentArticle = '20',
+      cashOrNot = true,
+      firstName = 'Jhon',
+      surname = 'Smith',
+      secondName = '123';
+
+
+console.log(ourPlanet, currentArticle, currentUserTime, cashOrNot);
